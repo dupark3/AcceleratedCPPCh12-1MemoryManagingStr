40b74278afa6485deb34bce4cc88999bf4437aec
Du Park <dupark3@gmail.com>
2018-03-16 10:20:55 -0400

    Moved member function implementation to cpp file. Added uncreate, clear, append, grow, unchecked_append member functions

--- Str.cpp
+++ Str.cpp
@@ -1,7 +1,64 @@
 
 
 #include <iostream>
+#include <memory>
 #include "Str.h"
+
+void Str::create(){
+    data = last = limit = 0;
+    arraySize = 0;
+}
+
+void Str::create(size_t n, const char& c){
+    arraySize = n;
+    data = alloc.allocate(arraySize);
+    last = limit = data + arraySize;
+    std::uninitialized_fill(data, last, c);
+}
+
+void Str::create(const char* cp){
+    arraySize = std::strlen(cp);
+    data = alloc.allocate(arraySize);
+    last = limit = data + arraySize;
+    std::uninitialized_copy(cp, cp + arraySize, data);
+}
+
+void Str::uncreate(){
+    if (data){
+        clear();
+        alloc.deallocate(data, limit - data);
+    }
+    data = last = limit = 0;
+}
+
+void clear(){
+    if (data){
+        char* it = last;
+        while (it != data)
+            alloc.destroy(--it);
+    }
+    last = data;
+}
+
+void append(const char& c){
+    if (last == limit)
+        grow();
+    unchecked_append(c);
+}
+
+void grow(){
+    size_t new_size = std::max(2 * (limit - data), ptrdiff_t(1));
+    char* new_data = alloc.allocate(new_size);
+    char* new_last = uninitialized_copy(data, last, new_data);
+    uncreate();
+    data = new_data;
+    last = new_last;
+    limit = data + new_size;
+}
+
+void unchecked_append(const char& c){
+    alloc.construct(last++, c);
+}
 
 // OUTPUT NONMEMBER FUNCTION
 std::ostream& operator<<(std::ostream& os, const Str& s){
@@ -14,7 +71,7 @@ std::ostream& operator<<(std::ostream& os, const Str& s){
 // INPUT FRIEND FUNCTION (gains access to private member data for writing)
 std::istream& operator>>(std::istream& is, Str& s){
     // obliterate existing values
-    s.data.clear();
+    s.clear();
 
     // read nad discard leading whitespace
     char c;
@@ -23,8 +80,10 @@ std::istream& operator>>(std::istream& is, Str& s){
 
     // if something still to read, do so until next whitespace character
     if (is){
-        do s.data.push_back(c);
-        while(is.get(c) && !isspace(c));
+        for (size_t i = 0; i != )
+        do {
+            data =
+        } while(is.get(c) && !isspace(c));
 
         // if we read whitspace, then put it back on the stream
         if (is)
@@ -41,3 +100,5 @@ Str& operator+ (const Str& first, const Str& second){
     ret += second;
     return ret;
 }
+
+
