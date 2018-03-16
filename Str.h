9a1e7ca1f20dbfd931783262a1f373c9759fab98
Du Park <dupark3@gmail.com>
2018-03-16 10:22:14 -0400

    Added declarations for member functions size, begin, end, create, uncreate, append

--- Str.h
+++ Str.h
@@ -1,11 +1,12 @@
-
 #ifndef GUARD_Str_h
 #define GUARD_Str_h
 
 #include <algorithm> // copy
+#include <cstddef> // size_t
 #include <cstring> // strlen
 #include <iostream>
 #include <iterator> // back_inserter
+#include <memory> // allocator
 
 
 class Str{
@@ -13,36 +14,53 @@ public:
 typedef size_t size_type;
 
     // CONSTRUCTORS
-    Str() { }
-    Str(size_t n, char c) : data(n, c) { }
-    Str(const char* cp) {
-        std::copy(cp, cp+ std::strlen(cp), std::back_inserter(data));
-    }
+    Str() { create(); }
+    explicit Str(size_t n, const char& c = char()) { create(n, c); }
+    Str(const char* cp) { create(cp); }
     template <class In> Str(In b, In e) {
         std::copy(b, e, std::back_inserter(data));
     }
+    ~Str() { uncreate() }
 
     // OPERATOR OVERLOADS
     char& operator[](size_t i) { return data[i]; }
     const char& operator[](size_t i) const { return data[i]; }
     friend std::istream& operator>>(std::istream&, Str&);
     Str& operator+=(const Str& rvalue) {
-        std::copy(rvalue.data.begin(), rvalue.data.end(), std::back_inserter(data));
+        char* temp = alloc.allocate(arraySize + rvalue.size());
+        std::copy(data, data + arraySize, temp);
+        data = temp;
+        std::copy(rvalue.begin(), rvalue.end(), data + arraySize);
         return *this;
-        /*for (size_t i = 0; i != rvalue.size(); ++i)
-            data.push_back(rvalue(i));*/
     }
 
     // MEMBER FUNCTIONS
-    size_t size() { return data.size(); }
-    const size_t size() const { return data.size(); }
-    char* begin() { return data.begin(); }
-    const char* begin() const { return data.begin(); }
-    char* end() { return data.end(); }
-    const char* end() const { return data.end(); }
+    size_t size() { return size(); }
+    const size_t size() const { return size(); }
+    char* begin() { return data; }
+    const char* begin() const { return data; }
+    char* end() { return data + arraySize; }
+    const char* end() const { return data + arraySize; }
+
+    void clear();
+
+    void append(const char&);
+
+    }
 
 private:
-    Vec<char> data;
+    char* data;
+    char* last;
+    char* limit;
+    size_t arraySize;
+    std::allocator<char> alloc;
+
+    void create();
+    void create(size_t, const char&);
+    void create(const char*);
+    void uncreate();
+    void grow();
+    void unchecked_appened(const char&);
 };
 
 // OUTPUT NONMEMBER FUNCTION
