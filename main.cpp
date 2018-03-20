6de7776d12419a73b670609a0b1a4ca2b9d92059
Du Park <dupark3@gmail.com>
2018-03-20 08:48:25 -0400

    Removed comments at the top of main source file and added it to a README file

--- main.cpp
+++ main.cpp
@@ -1,38 +1,3 @@
-/* Accelerated C++ Exercise 12-0
-Implement the Str class that simplifies the string class as defined by the
-standard library. */
-
-/*  Accelerated C++ Exercise 12-1
-Implement the Str class, but choose an implementation strategy that requires
-that the class manage the storage itself. For example, you might store an
-array of char and a length. */
-
-/* Accelerated C++ Exercise 12-2
-Implement the c_str, data, and copy functions that the string library
-implements in order to convert a string into a null-terminated char array
-without causing memory management issues */
-
-/* Accelerated C++ Exercise 12-3 and 12-4
-Define the relational, equality, and inequality operators for Str using strcmp
-*/
-
-/* Accelerated C++ Exercise 12-5
-Implement concatenation for Str without relying on conversions from const
-char* */
-
-/* Accelerated C++ Exercise 12-6
-Give Str an operation that will let us implicitly use a Str object as a
-condition. The test should fail if the Str is empty, and should succeed
-otherwise. */
-
-/* Accelerated C++ Exercise 12-7
-Add iterators and the iterator operations begin and end to your Str class,
-similar to how the string class provides random access iterators to manipulate
-the string's characters. */
-
-/* Accelerated C++ Exercise 12-8
-Add the getline function to the Str class */
-
 #include <cstring> // strlen
 #include <iostream>
 
